5edecbf7f0d65f314cf022a40f94d3f868b7f480
DevOps Insiders <info@devopsinsiders.com>
2025-07-11 22:25:14 +0530

    removed something

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -4,18 +4,6 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
-module "resource_group2" {
-  source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "102-rg-india"
-  resource_group_location = "centralindia"
-}
-
-module "resource_group1" {
-  source = "../modules/azurerm_resource_group"
-  resource_group_name = "rg-canada"
-  resource_group_location = "canada central"
-}
-
 module "virtual_network" {
   depends_on = [module.resource_group]
   source     = "../modules/azurerm_virtual_network"
